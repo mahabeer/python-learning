f2dd66692c5dd86f13df7d0caf9cba8d3fb0b55b
root <root@MahabeerSingh>
2024-05-31 20:08:04 +0530

    first commit

---
+++ 1-data-types.py
@@ -0,0 +1,31 @@
+# Python is dynamically typed, thats means we dont need to define type for variable while declaring
+
+# string datatype
+name="John Doe"
+print(type(name))
+print(name)
+
+# int datatype
+age=27
+print(type(age))
+print(age)
+
+# float datatype
+height=5.9
+print(type(height))
+print(height)
+
+# bool datatype
+isEligible = False
+print(type(isEligible))
+print(isEligible)
+
+# NoneType datatype
+noValue=None
+print(type(noValue))
+print(noValue)
+
+# input method Reads a string from standard input.
+num1 = input("Enter you first number ")
+num2 = input("Enter you second number ")
+print(int(num1)+int(num2))
